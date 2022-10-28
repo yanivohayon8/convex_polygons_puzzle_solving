18eb3f147f5bd5e5a3c2765e884968009098098c
Yaniv Ohayon <yanivohayon8@gmail.com>
2022-10-28 16:15:31 +0300

    ofir loader

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+if __name__ == "__main__":
+    file_path = "data/ofir/"
+    puzzle = pd.read_csv()
+
+    
---
+++ src/dataset_loader.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+class Ofir():
+
+    def __init__(self,puzzle_path,rels_path,pieces_path) -> None:
+        '''
+            puzzle_path - the file path of the csv file ground_truth_puzzle.csv
+            rels_path - the file path of the csv file ground_truth_rels.csv
+            pieces_path - the file path of the csv file pieces.csv
+        '''
+        self.puzzle_path = puzzle_path
+        self.rels_path = rels_path
+        self.pieces_path = pieces_path
+        self.df_puzzle = None
+        self.df_rels = None
+        self.df_pieces = None
+
+    def load(self):
+        self.df_puzzle = pd.read_csv(self.puzzle_path)
+        self.df_rels = pd.read_csv(self.rels_path)
+        self.df_pieces = pd.read_csv(self.pieces_path)
