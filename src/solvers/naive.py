f9c26aa9e3801e19076ef313b3e16596e3de18cf
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-07-15 14:52:33 +0300

    delete do nothing

--- src/solvers/naive.py
+++ src/solvers/naive.py
@@ -301,44 +301,4 @@ class PictorialSolver(GeometricNoiselessSolver):
             break
     
             
-        
-
-                
-        
-            
-        
-      
-                
-                
-            
-
-
-class DoNothing():
-    
-    def __init__(self,pieces:list):
-        self.pieces = pieces
-
-
-    '''we need an option of live stream video of the assembly for debugging'''
-    def run(self):
-        edge_index = 1
-        pieceAs = []
-        pieceBs = []
-        edge1s = []
-        edge2s = []
-        for piece_index in range(len(self.pieces)-1):
-            pieceAs.append(self.pieces[piece_index])
-            pieceBs.append(self.pieces[piece_index+1])
-            edge1s.append(edge_index%2)
-            edge2s.append((edge_index+1)%2)
-
-        # df_adjacency_relations = pd.Dataframe({
-        #     "pieceA":pieceAs,
-        #     "pieceB":pieceBs,
-        #     "edgeA":edge1s,
-        #     "edgeB":edge2s
-        # })
-        df_adjacency_relations = None
-
-        return Assembly(df_adjacency_relations,self.pieces)
 
