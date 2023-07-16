0c668b298365230485cb6c40c906bba92350a187
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-07-16 09:26:56 +0300

    test reconstruct

--- tests/test_http_client.py
+++ tests/test_http_client.py
@@ -13,6 +13,12 @@ class TestHTTPClient(unittest.TestCase):
         http = HTTPClient("Pseudo-Sappho_MAN_Napoli_Inv9084",1,0)
         print(http.send_sanity())
 
+    def test_reconstruct_Inv9084_num1_noise0(self):
+        http = HTTPClient("Pseudo-Sappho_MAN_Napoli_Inv9084",1,0)
+        encoded_data = "0,0,1,0\r\n0,0,1,0\r\n0,1,1,2\r\n1,1,2,0\r\n1,2,2,2\r\n2,1,5,1\r\n2,2,5,0\r\n"
+        
+        print(http.send_reconstruct_request(encoded_data))
+
 
 
 if __name__ == "__main__":
