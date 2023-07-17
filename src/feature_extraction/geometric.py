bde114b80d426f992dc8a811c997c9265f0faf00
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-07-17 14:51:27 +0300

    Geometric Extractors in new format and test

--- src/feature_extraction/geometric.py
+++ src/feature_extraction/geometric.py
@@ -1,7 +1,60 @@
 from shapely import Polygon
 import numpy as np
+from src.piece import Piece
+
+class Extractor():
+    def __init__(self, pieces):
+        self.pieces = pieces
+    
+    def extract_for_piece(self,piece:Piece):
+        raise ("Implement Me")
+
+    def run(self):
+        for piece in self.pieces:
+            self.extract_for_piece(piece)
+
+class EdgeLengthExtractor(Extractor):
+
+    def _get_polygon_edges_lengths(self,coords):
+        coords_prev = np.array(coords[:-1]).reshape(-1,2)
+        coords_next = np.array(coords[1:]).reshape(-1,2)
+        return np.sqrt(np.sum((coords_next-coords_prev)**2,axis=1))
+    
+    def extract_for_piece(self,piece:Piece):
+        piece.features["edges_length"] =  self._get_polygon_edges_lengths(piece.get_coords())
+    
+
+class AngleLengthExtractor(Extractor):  
+
+    def _get_angle_of_vectors_(self,coord_left,coord_middle,coord_right):
+        vector_1 = np.array(coord_left-coord_middle)
+        vector_2 = np.array(coord_right-coord_middle)
+        dot = vector_1.dot(vector_2)
+        denominator = (np.linalg.norm(vector_1) * np.linalg.norm(vector_2))
+        return np.arccos(dot/denominator)
+
+    def _get_polygon_angles(self,coords):
+        num_coords = coords.shape[0]-1
+        polygon_angles = [
+            np.degrees(
+                self._get_angle_of_vectors_(coords[(i-1)%num_coords],
+                                           coords[i],
+                                           coords[(i+1)%num_coords])
+                ) for i in range(num_coords)
+        ]
+        
+        return polygon_angles
+    
+    def extract_for_piece(self,piece:Piece):
+        piece.features["angles"] = self._get_polygon_angles(np.array(piece.get_coords()))
 
 
+
+'''
+
+    Old:
+
+'''
 class GeometricFeatureExtractor():
 
     def __init__(self) -> None:
--- tests/test_feature_extraction.py
+++ tests/test_feature_extraction.py
@@ -4,8 +4,10 @@ from src.piece import Piece
 import matplotlib.pyplot as plt
 from src.feature_extraction.pictorial import slice_image,rotate_and_crop,trans_image
 import numpy as np
+from src.feature_extraction import geometric as geo_extractor 
 
-class TestPictorialFeatureExtractor(unittest.TestCase):
+
+class TestPictorial(unittest.TestCase):
     
     def test_slice_image_func(self):
         puzzleDirectory = "data/ofir/Pseudo-Sappho_MAN_Napoli_Inv9084/Puzzle1/0"
@@ -135,7 +137,31 @@ class TestPictorialFeatureExtractor(unittest.TestCase):
         axs[1,0].imshow(image)
         plt.show()
 
-        
+
+class TestGeometric(unittest.TestCase):
+
+
+    def test_edge_length(self):
+        pieces = [
+            Piece("0",[(-847.4256148049196,409.07035256641507),(-390.6580493044287,580.180048490992),(1039.668350940752,-119.88084521481937),(198.41531316859664,-869.3695558425879)])
+        ]
+
+        length_extractor = geo_extractor.EdgeLengthExtractor(pieces)
+        length_extractor.run()
+        print(pieces[0].features["edges_length"])
+    
+    def test_angle(self):
+        pieces = [
+            Piece("0",[(-847.4256148049196,409.07035256641507),(-390.6580493044287,580.180048490992),(1039.668350940752,-119.88084521481937),(198.41531316859664,-869.3695558425879)])
+        ]
+
+        angles_extractor = geo_extractor.AngleLengthExtractor(pieces)
+        angles_extractor.run()
+
+        print(pieces[0].features["angles"])
+    
+
+
 
 if __name__ == "__main__":
     unittest.main()
