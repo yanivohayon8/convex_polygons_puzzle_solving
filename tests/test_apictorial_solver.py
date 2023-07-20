6c53bb0d98cca36dcac4e118a9db761d071077c0
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-07-20 10:52:06 +0300

    adding more test of different noises

--- tests/test_apictorial_solver.py
+++ tests/test_apictorial_solver.py
@@ -30,7 +30,47 @@ class TestIntegration(unittest.TestCase):
 
         self._run(image,puzzle_num,puzzle_noise_level)
 
-        
+    def test_Inv9084_puzzle_1_noise_1(self):
+        image = "Pseudo-Sappho_MAN_Napoli_Inv9084"
+        puzzle_num = 1
+        puzzle_noise_level = 1
+
+        self._run(image,puzzle_num,puzzle_noise_level)
+    
+    def test_Inv9084_puzzle_1_noise_2(self):
+        image = "Pseudo-Sappho_MAN_Napoli_Inv9084"
+        puzzle_num = 1
+        puzzle_noise_level = 2
+
+        self._run(image,puzzle_num,puzzle_noise_level)
+
+    def test_Inv9084_puzzle_1_noise_3(self):
+        image = "Pseudo-Sappho_MAN_Napoli_Inv9084"
+        puzzle_num = 1
+        puzzle_noise_level = 3
+
+        self._run(image,puzzle_num,puzzle_noise_level)
+    
+    def test_Inv9084_puzzle_1_noise_4(self):
+        image = "Pseudo-Sappho_MAN_Napoli_Inv9084"
+        puzzle_num = 1
+        puzzle_noise_level = 4
+
+        self._run(image,puzzle_num,puzzle_noise_level)
+
+    def test_Inv9084_puzzle_1_noise_5(self):
+        image = "Pseudo-Sappho_MAN_Napoli_Inv9084"
+        puzzle_num = 1
+        puzzle_noise_level = 5
+
+        self._run(image,puzzle_num,puzzle_noise_level)
+
+    def test_Inv9084_puzzle_1_noise_6(self):
+        image = "Pseudo-Sappho_MAN_Napoli_Inv9084"
+        puzzle_num = 1
+        puzzle_noise_level = 6
+
+        self._run(image,puzzle_num,puzzle_noise_level)
 
 
 
