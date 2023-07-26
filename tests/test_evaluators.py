a4898eadc3a508e1685f3dd94f001096e3a835aa
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-07-26 11:47:45 +0300

    adding test

--- tests/test_evaluators.py
+++ tests/test_evaluators.py
@@ -25,6 +25,29 @@ class TestLeastSquareRigidBody(unittest.TestCase):
         assert np.array_equal(t,np.array([-2,-2]))
         assert np.array_equal(R,np.identity(2))
 
+    def test_only_rotation_needed(self):
+        points = np.array([
+            [1,0],
+            [1,1],
+            [0,1],
+            [0,0]
+        ])
+
+        ground_truth = np.array([
+            [0,0],
+            [1,0],
+            [1,1],
+            [0,1]
+        ])
+
+        weights = np.array([1,1,1,1])
+
+        R,t = least_square_rigid_motion_svd(points,weights,ground_truth)
+        assert np.array_equal(t,np.array([0,0]))
+        assert np.array_equal(R,np.array([
+            [0,1],
+            [-1,0]
+        ]))
 
 if __name__ == "__main__":
     unittest.main()
