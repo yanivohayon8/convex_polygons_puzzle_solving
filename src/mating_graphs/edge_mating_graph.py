c3e72b5b4f67a746283b792068ab23ca636977b8
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-08-02 09:01:59 +0300

    cycles in another module

--- src/mating_graphs/edge_mating_graph.py
+++ src/mating_graphs/edge_mating_graph.py
@@ -3,45 +3,9 @@ from src.data_structures import Mating
 import matplotlib.pyplot as plt
 import matplotlib.collections as mpc
 import math
+from src.mating_graphs.cycle import Cycle
 
 
-class Cycle():
-
-    def __init__(self, matings_chain:list, piece2occurence:dict) -> None:
-        self.matings_chain = matings_chain
-        self.piece2occurence = piece2occurence
-    
-    def get_pieces_involved(self):
-        return self.piece2occurence.keys()
-
-    def get_num_pieces(self):
-        return len(self.piece2occurence.keys())
-
-    def is_all_piece_occur(self,occurence_num):
-        for piece_id in self.piece2occurence.keys():
-            
-            if self.piece2occurence[piece_id] != occurence_num:
-                return  False
-        
-        return True
-
-    def is_has_piece_duplicate_occurence(self):
-
-        for piece_id in self.piece2occurence.keys():
-            
-            if self.piece2occurence[piece_id] > 2:
-                return  True
-        
-        return False
-    
-    def __repr__(self) -> str:
-        acc = ""
-        delimiter = "==>"
-
-        for mate in self.matings_chain:
-            acc = acc + delimiter + repr(mate)
-        
-        return acc[len(delimiter):]
 
 
 
