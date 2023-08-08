c0caefbdc15a446846f65b89c49e901ba29c1cec
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-08-08 14:51:08 +0300

    plotting nicely

--- tests/test_mating_graph_drawer.py
+++ tests/test_mating_graph_drawer.py
@@ -31,29 +31,50 @@ class TestGraphDrawer(unittest.TestCase):
 
         return graph
 
+    def _draw(self,graph,ground_truth_graph,ax1,ax2):
+        drawer = MatchingGraphDrawer(ground_truth_graph)
+        drawer.init()
+
+        drawer.draw_adjacency_graph(graph,ax=ax1)
+        ax1.set_title("Noised")
+
+        drawer.draw_adjacency_graph(ground_truth_graph,ax=ax2)
+        ax2.set_title("Noiseless")
+        # drawer.draw_graph_matching(graph)
+
+        
+
     def test_draw_ground_truth(self):
         puzzle_image = "Pseudo-Sappho_MAN_Napoli_Inv9084"
-        puzzle_num = 1
+        puzzle_num = 3
         ground_truth_graph = self._load_graph(puzzle_image,puzzle_num,0)
         drawer = MatchingGraphDrawer(ground_truth_graph)
         drawer._draw_ground_truth_adjacency()
-        drawer._draw_ground_truth_matching()
+        # drawer._draw_ground_truth_matching()
         plt.show()
     
-    def test_draw_graph_with_noise(self):
+    def test_draw_Inv9084_with_noise(self):
         puzzle_image = "Pseudo-Sappho_MAN_Napoli_Inv9084"
-        puzzle_num = 1
+        puzzle_num = 2
+        puzzle_noise_level = 1
+        
+        ground_truth_graph = self._load_graph(puzzle_image,puzzle_num,0)
+        graph = self._load_graph(puzzle_image,puzzle_num,puzzle_noise_level)
+
+        fig, axs = plt.subplots(1,2)
+        self._draw(graph,ground_truth_graph,axs[0],axs[1])
+        plt.show()
+    
+    def test_VilladeiMisteri_puzzle_1(self,puzzle_noise_level = 0):
+        puzzle_image = "Roman_fresco_Villa_dei_Misteri_Pompeii_009"
+        puzzle_num = 2
         puzzle_noise_level = 1
         
         ground_truth_graph = self._load_graph(puzzle_image,puzzle_num,0)
         graph = self._load_graph(puzzle_image,puzzle_num,puzzle_noise_level)
         
-        drawer = MatchingGraphDrawer(ground_truth_graph)
-        drawer.init()
-
-        drawer.draw_adjacency_graph(graph)
-        drawer.draw_graph_matching(graph)
-
+        fig, axs = plt.subplots(1,2)
+        self._draw(graph,ground_truth_graph,axs[0],axs[1])
         plt.show()
 
 if __name__ == "__main__":
