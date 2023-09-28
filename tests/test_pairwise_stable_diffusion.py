aa86cd8425d3cf78cee65e70b9bff7c634ad0e1b
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-09-28 12:16:45 +0300

    load_bag_of_pieces

--- tests/test_pairwise_stable_diffusion.py
+++ tests/test_pairwise_stable_diffusion.py
@@ -6,6 +6,17 @@ from src.feature_extraction import image_process
 from src.feature_extraction.extrapolator.stable_diffusion import SDExtrapolatorExtractor,SDOriginalExtractor
 from src.pairwise_matchers.stable_diffusion import DotProductExtraToOriginalMatcher
 
+def load_bag_of_pieces(db,puzzle_num,puzzle_noise_level):
+    puzzle = Puzzle(f"../ConvexDrawingDataset/DB{db}/Puzzle{puzzle_num}/noise_{puzzle_noise_level}")
+    puzzle.load()
+    bag_of_pieces = puzzle.get_bag_of_pieces()
+
+    for piece in bag_of_pieces:
+        piece.load_extrapolated_image()
+        piece.load_stable_diffusion_original_image()
+    
+    return bag_of_pieces
+
 class TestFunction_score_pair(unittest.TestCase):
 
     def _load_chosen_pieces_pair(self,piece_ii,piece_jj,db = 1,puzzle_num = 19,puzzle_noise_level = 0):
@@ -106,14 +117,7 @@ class TestFunction_score_pair(unittest.TestCase):
         db = 1
         puzzle_num = 19
         puzzle_noise_level = 0
-
-        puzzle = Puzzle(f"../ConvexDrawingDataset/DB{db}/Puzzle{puzzle_num}/noise_{puzzle_noise_level}")
-        puzzle.load()
-        bag_of_pieces = puzzle.get_bag_of_pieces()
-        
-        for piece in bag_of_pieces:
-            piece.load_extrapolated_image()
-            piece.load_stable_diffusion_original_image()
+        bag_of_pieces = load_bag_of_pieces(db,puzzle_num,puzzle_noise_level)
 
         chosen_pieces = [bag_of_pieces[piece_ii],bag_of_pieces[piece_jj]]
 
@@ -208,56 +212,10 @@ class TestFunction_score_pair(unittest.TestCase):
         print(f"CONCLUSION: {winner} > {loser}")
 
 
+class TestFunctionPairwise(unittest.TestCase):
     
-
-    # def test_normalized_image_pair_best_buddies(self,piece_ii = 1,edge_ii = 1,
-    #                                        piece_jj =5,edge_jj = 2,
-    #                                        sample_height=5):
-    #     db = 1
-    #     puzzle_num = 19
-    #     puzzle_noise_level = 0
-    #     puzzle = Puzzle(f"../ConvexDrawingDataset/DB{db}/Puzzle{puzzle_num}/noise_{puzzle_noise_level}")
-    #     puzzle.load()
-    #     bag_of_pieces = puzzle.get_bag_of_pieces()
-    #     chosen_pieces = [bag_of_pieces[piece_ii],bag_of_pieces[piece_jj]]
-        
-    #     for piece in chosen_pieces:
-    #         piece.load_extrapolated_image()
-    #         piece.load_stable_diffusion_original_image()
-
-    #     extrapolation_extractor = NormalizeSDExtrapolatorExtractor(chosen_pieces,extrapolation_height=sample_height)
-    #     extrapolation_extractor.run()
-
-    #     original_extractor = NormalizeSDOriginalExtractor(chosen_pieces,sampling_height=sample_height)
-    #     original_extractor.run()
-
-    #     pictorial_matcher = DotProductExtraToOriginalMatcher(chosen_pieces,
-    #                                                          extrapolation_extractor.__class__.__name__,
-    #                                                          original_extractor.__class__.__name__)
-
-    #     pictorial_matcher.pairwise()
-
-    #     edge_ii_img_extra_same = chosen_pieces[0].features[extrapolation_extractor.__class__.__name__][edge_ii]["same"]
-    #     edge_ii_img_flipped = chosen_pieces[0].features[original_extractor.__class__.__name__][edge_ii]["flipped"]
-    #     edge_jj_img_extra_same = chosen_pieces[1].features[extrapolation_extractor.__class__.__name__][edge_jj]["same"]
-    #     edge_jj_img_flipped = chosen_pieces[1].features[original_extractor.__class__.__name__][edge_jj]["flipped"]
-        
-        
-
-    #     fig, axs = plt.subplots(2,2)
-    #     fig.suptitle(f"Score: {pictorial_matcher.get_score(piece_ii,edge_ii,piece_jj,edge_jj)}")
-        
-    #     '''VERIFY THIS PLOTTING...'''
-    #     axs[0,0].set_title(f"P_{piece_jj}_E_{edge_jj} Original (FLIPPED)")
-    #     axs[0,0].imshow(edge_jj_img_flipped.astype(np.int))
-    #     axs[0,1].set_title(f"P_{piece_ii}_E_{edge_ii} Extrapolated (SAME)")
-    #     axs[0,1].imshow(edge_ii_img_extra_same.astype(np.int)) 
-    #     axs[1,0].set_title(f"P_{piece_ii}_E_{edge_ii} Original (FLIPPED)")
-    #     axs[1,0].imshow(edge_ii_img_flipped.astype(np.int)) 
-    #     axs[1,1].set_title(f"P_{piece_jj}_E_{edge_jj} Extrapolated (SAME)")
-    #     axs[1,1].imshow(edge_jj_img_extra_same.astype(np.int))
-
-    #     plt.show()
+    def _pairwise_puzzle(self,db,puzzle_num,puzzle_noise_level):
+        bag_of_pieces = load_bag_of_pieces(db,puzzle_num,puzzle_noise_level)
 
 
 
