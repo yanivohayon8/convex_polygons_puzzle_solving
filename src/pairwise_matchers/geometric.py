724acff17f2af857655fd3eba06ef852aba31c24
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-10-16 18:15:59 +0300

    delete useless class

--- src/pairwise_matchers/geometric.py
+++ src/pairwise_matchers/geometric.py
@@ -1,7 +1,5 @@
 
 import numpy as np
-from src.pairwise_matchers import PairwiseMatcher
-import matplotlib.pyplot as plt
 from src.mating import Mating
 
 class EdgeMatcher():
@@ -58,79 +56,3 @@ class EdgeMatcher():
         
         return matings
                         
-''' 
-    Old 
-'''
-class GeometricPairwiseMatcher(PairwiseMatcher):
-
-    def __init__(self) -> None:
-        super(GeometricPairwiseMatcher,self).__init__()
-        self.match_edges = {}
-
-    def pairwise_edges_lengths(self,edge_lengths:np.array,confidence_interval=1.0):
-        num_pieces = len(edge_lengths)
-        matching_edges = [[] for _ in range(num_pieces**2)]
-        matching_scores = [[] for _ in range(num_pieces**2)]
-
-        for i in range(num_pieces):
-            for j in range(num_pieces):
-                if i!=j:
-                    piece_i = edge_lengths[i].reshape(-1,1)
-                    piece_j = edge_lengths[j].reshape(1,-1)
-                    subs = np.abs(piece_i-piece_j) # tiling
-                    match_edges_diff = subs[subs<confidence_interval]
-
-                    if match_edges_diff.size > 0:
-                        matching_edges[i*num_pieces+j].append(np.argwhere(subs<confidence_interval))
-                        matching_scores[i*num_pieces+j] = match_edges_diff
-
-        self.match_edges = np.array(matching_edges,dtype="object").reshape((num_pieces,num_pieces))
-        self.match_pieces_score = np.array(matching_scores,dtype="object").reshape((num_pieces,num_pieces))
-    
-
-    def adjacency_matrix(self):
-        self.piece_adj_mat = np.zeros(self.match_pieces_score.shape[:2])
-        num_pieces = self.match_pieces_score.shape[0]
-        for i in range(num_pieces):
-            for j in range(num_pieces):
-                
-                if i == j:
-                    # self.piece_adj_mat[i,j] = 0
-                    continue
-
-                matching = self.match_pieces_score[i,j]
-                if len(matching) > 0:
-                    self.piece_adj_mat[i,j] = np.min(matching) #5/(np.min(self.match_pieces_score[i,j]) + 1e-1)# this is the difference from parent class
-                    continue
-
-        # self.piece_adj_mat*=0.1
-        self.piece_adj_mat = (self.piece_adj_mat-np.min(self.piece_adj_mat))/(np.max(self.piece_adj_mat)-np.min(self.piece_adj_mat))
-        self.piece_adj_mat = np.where(self.piece_adj_mat>0,1-self.piece_adj_mat,0)
-
-        return self.piece_adj_mat
-    
-    def plot_heat_map(self,ax,fig,pieces_labels=None):
-        ax.imshow(self.piece_adj_mat,cmap="hot") #alpha=0.7 ,cmap="hot"
-
-        num_pieces = self.piece_adj_mat.shape[0]
-        ax.set_xticks(np.arange(num_pieces),labels=pieces_labels) # , labels=
-        ax.set_yticks(np.arange(num_pieces),labels=pieces_labels) # , labels=
-
-        plt.setp(ax.get_xticklabels(),rotation=45,ha="right",rotation_mode="anchor")
-        
-        for piece_i in range(num_pieces):
-            for piece_j in range(num_pieces):
-                val = round(self.piece_adj_mat[piece_i,piece_j],3)
-
-                text_color = 'black' if val > 0.5 else 'white'
-
-                ax.text(piece_j,piece_i, val ,
-                        ha="center",va="center",color=text_color)
-        
-        ax.set_title("Piece adjacency heat map")
-        fig.tight_layout()
-
-    
-
-
-                
--- tests/test_pairwiser.py
+++ tests/test_pairwiser.py
@@ -1,5 +1,5 @@
 import unittest 
-from src.pairwise_matchers.geometric import GeometricPairwiseMatcher,EdgeMatcher
+from src.pairwise_matchers.geometric import EdgeMatcher
 import numpy as np
 import matplotlib.pyplot as plt
 from src.feature_extraction.geometric import EdgeLengthExtractor
@@ -258,62 +258,6 @@ class TestEdgeMatcher(unittest.TestCase):
         
         assert len(matings) == 4
 
-
-'''
-Old# Todo: write unit test for EdgeMatcher class
-
-'''
-class TestGeometric_Old(unittest.TestCase):
-    
-    def test_Inv9084_puzzle_1_noise_0(self):
-        edges_lengths = np.array([
-            np.array([1652.17413843, 1129.10319381, 1595.22517657,  488.18193589]),
-            np.array([488.18193145, 806.35067968, 973.67087051]),
-            np.array([806.35069792, 160.42254484, 692.30414656]),
-            np.array([ 692.30415948,  756.87697074, 1235.6577307 ]),
-            np.array([1235.65771389, 1024.56403487, 2220.85045619]),
-            np.array([1595.22519874, 1022.96451656,  756.87695689,  160.4225449 ]),
-            np.array([1022.96451726,  748.60088725, 1024.56403076]),
-            np.array([1062.97593227,  674.18139265,  932.33867735]),
-            np.array([ 932.33868471,  264.66809436, 1129.10323532]),
-            np.array([ 674.18138322, 1527.09691397,  748.60088748,  264.66808857])
-
-        ])
-
-        noise = 1e-3
-
-        pairwiser = GeometricPairwiseMatcher()
-        pairwiser.pairwise_edges_lengths(edges_lengths,confidence_interval=noise)
-        pairwiser.adjacency_matrix()
-        fig,ax = plt.subplots()
-        pairwiser.plot_heat_map(ax,fig)
-        plt.waitforbuttonpress()
-
-    def test_Inv9084_puzzle_1_noise_1(self):
-        edges_lengths = np.array([
-            np.array([1651.72390127, 1126.69428015, 1592.45686476,  487.76545279]),
-            np.array([485.10522711, 806.04903711, 971.39228994]),
-            np.array([803.04910143, 158.7807397 , 690.41483373]),
-            np.array([ 690.09324581,  756.04213798, 1235.20247625]),
-            np.array([1236.17184229, 1020.47208218, 2217.49155725]),
-            np.array([1593.71659059, 1021.13843792,  756.43003847,  159.33799994]),
-            np.array([1020.44830287,  746.35767875, 1020.9147307 ]),
-            np.array([1057.77093195,  670.54278589,  929.15343577]),
-            np.array([ 930.18667907,  263.89589441, 1127.21116365]),
-            np.array([ 673.26585727, 1524.74870568,  747.75346429,  263.26379564])
-        ])
-
-
-        puzzle_diameter = 3007.6720313778787
-        xi = 3.007672031377879
-        noise = xi#*puzzle_diameter/100
-
-        pairwiser = GeometricPairwiseMatcher()
-        pairwiser.pairwise_edges_lengths(edges_lengths,confidence_interval=noise)
-        pairwiser.adjacency_matrix()
-        fig,ax = plt.subplots()
-        pairwiser.plot_heat_map(ax,fig)
-        plt.waitforbuttonpress()
         
 
 if __name__ == "__main__":
