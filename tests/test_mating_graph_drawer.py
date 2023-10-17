1d5bccd38a255345a8843a7e6dfb1908241a094a
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-10-17 18:44:24 +0300

    refactor test_mating_graph_drawer

--- tests/test_mating_graph_drawer.py
+++ tests/test_mating_graph_drawer.py
@@ -13,117 +13,83 @@ from src.feature_extraction import extract_features
 
 class TestGraphDrawer(unittest.TestCase):
 
-    def _load_graph(self,db,puzzle_num,puzzle_noise_level,
-                    features,
-                    pictorial_matcher="DotProductNoisslessMatcher"):
+
+    def test_draw_adjacency(self):
+        db = "1"
+        puzzle_num = "19"
         
-        bag_of_pieces = recipes_factory.create("loadRegularPuzzle",
-                                                db=db,puzzle_num=puzzle_num,noise_level=puzzle_noise_level).cook()
-        extract_features(bag_of_pieces,features)
+        gd_puzzle_recipe = recipes_factory.create("loadRegularPuzzle",
+                                                db=db,puzzle_num=puzzle_num,noise_level=0)
+        gd_puzzle_recipe.cook()
+        gd_recipe = recipes_factory.create("SD1Pairwise",puzzle=gd_puzzle_recipe.puzzle)
+        gd_graph_wrapper = gd_recipe.cook()
 
+        noise_level = 1
+        noisy_puzzle_recipe = recipes_factory.create("loadRegularPuzzle",
+                                                db=db,puzzle_num=puzzle_num,noise_level=noise_level)
+        noisy_puzzle_recipe.cook()
+        noisy_recipe = recipes_factory.create("SD1Pairwise",puzzle=noisy_puzzle_recipe.puzzle)
+        noisy_graph_wrapper = noisy_recipe.cook()
 
-
-
-        # if "length" in  geo_feature_extractors:
-        #     edge_length_extractor = geo_extractor.EdgeLengthExtractor(bag_of_pieces)
-        #     edge_length_extractor.run()
-
-        # if "angle" in geo_feature_extractors:
-        #     angles_extractor = geo_extractor.AngleLengthExtractor(bag_of_pieces)
-        #     angles_extractor.run()
-
-        edge_length_pairwiser = EdgeMatcher(bag_of_pieces)
-        edge_length_pairwiser.pairwise(puzzle.matings_max_difference+1e-3)
-
-        # pic_extractor = EdgePictorialExtractor(bag_of_pieces,sampling_height=10)
-        # pic_extractor = EdgePictorialAndNormalizeExtractor(bag_of_pieces,sampling_height=10)
-        # pic_extractor.run()
-        
-        # self.pictorial_matcher_ = DotProductNoisslessMatcher(bag_of_pieces,feature_name=pic_extractor.__class__.__name__)
-        # self.pictorial_matcher_.pairwise()
-
-
-        # sampling_height = 4
-        # extrapolator_extractor = NormalizeSDExtrapolatorExtractor(bag_of_pieces,extrapolation_height=sampling_height)
-        # extrapolator_extractor.run()
-
-        # original_extractor = NormalizeSDOriginalExtractor(bag_of_pieces,sampling_height=sampling_height)
-        # original_extractor.run()
-
-        # self.pictorial_matcher_ = DotProductExtraToOriginalMatcher(bag_of_pieces,
-        #                                                            extrapolator_extractor.__class__.__name__,
-        #                                                            original_extractor.__class__.__name__,
-        #                                                            step_size=50)
-        # self.pictorial_matcher_.pairwise()
-
-        # wrapper = MatchingGraphWrapper(bag_of_pieces,id2piece,
-        #                                         edge_length_pairwiser.match_edges,
-        #                                         edge_length_pairwiser.match_pieces_score,
-        #                                         pictorial_matcher=self.pictorial_matcher_)
-        # wrapper.build_graph()
-
-        # return wrapper
-
-    def _draw_adjacency(self,wrapper:MatchingGraphWrapper,ground_truth_wrapper:MatchingGraphWrapper,ax1,ax2):
-        drawer = MatchingGraphDrawer(ground_truth_wrapper)
+        drawer = MatchingGraphDrawer(gd_graph_wrapper)
         drawer.init()
 
-        drawer.draw_adjacency_graph(wrapper,ax=ax1)
-        ax1.set_title("Noised")
-        drawer.draw_adjacency_graph(ground_truth_wrapper,ax=ax2)
-        ax2.set_title("Noiseless")
-
-    def _draw_matching(self,wrapper:MatchingGraphWrapper,ground_truth_wrapper:MatchingGraphWrapper):
-        drawer = MatchingGraphDrawer(ground_truth_wrapper)
-        drawer.init()
-
-        # Because we you use the normalized dot product
-        min_edge_weight = -1
-        max_edge_weight = 1
-        drawer.draw_graph_matching(wrapper,min_edge_weight=min_edge_weight,max_edge_weight=max_edge_weight)
-        drawer.draw_graph_filtered_matching(wrapper,min_edge_weight=min_edge_weight,max_edge_weight=max_edge_weight)
-        # self.pictorial_matcher_.plot_scores_histogram()
-
-    def test_draw_pictorial_matches(self):
-        db = "1" 
-        puzzle_num = 19 #13 #20
-        features = ["AngleLengthExtractor","EdgeLengthExtractor",
-                    "NormalizeSDExtrapolatorExtractor","NormalizeSDOriginalExtractor"]
-
-        ground_truth_wrapper = self._load_graph(db,puzzle_num,0,features)
-        wrapper = self._load_graph(db,puzzle_num,0,features)
-
-        self._draw_matching(wrapper,ground_truth_wrapper)
-        # fig, axs = plt.subplots(1,2)
-        # self._draw_adjacency(wrapper,ground_truth_wrapper,axs[0],axs[1])
-       
-        plt.show()
-
-    def test_puzzle_with_missing_pieces(self):
-        db = "5" 
-        puzzle_num = 1 #13 #20
-
-        ground_truth_wrapper = self._load_graph(db,puzzle_num,0)
-        wrapper = self._load_graph(db,puzzle_num,1)
-
-        self._draw_matching(wrapper,ground_truth_wrapper)
         fig, axs = plt.subplots(1,2)
-        self._draw_adjacency(wrapper,ground_truth_wrapper,axs[0],axs[1])
-       
+
+        drawer.draw_adjacency_graph(noisy_graph_wrapper,ax=axs[0])
+        axs[0].set_title("Noised")
+        drawer.draw_adjacency_graph(gd_graph_wrapper,ax=axs[1])
+        axs[1].set_title("Noiseless")
+
         plt.show()
-
-
 
     def test_draw_ground_truth(self):
         db = "1"
         puzzle_num = 19
-        ground_truth_graph = self._load_graph(db,puzzle_num,0)
-        drawer = MatchingGraphDrawer(ground_truth_graph)
+        gd_noise_level = 0
+        
+        gd_puzzle_recipe = recipes_factory.create("loadRegularPuzzle",
+                                                db=db,puzzle_num=puzzle_num,noise_level=gd_noise_level)
+        gd_puzzle_recipe.cook()
+        gd_recipe = recipes_factory.create("SD1Pairwise",puzzle=gd_puzzle_recipe.puzzle)
+        gd_graph_wrapper = gd_recipe.cook()
+
+        drawer = MatchingGraphDrawer(gd_graph_wrapper)
         drawer._draw_ground_truth_adjacency()
-        # drawer._draw_ground_truth_matching()
         plt.show()
     
     
+    def test_draw_matching(self):
+        db = "1"
+        puzzle_num = "19"
+        gd_noise_level = 0
+        
+        gd_puzzle_recipe = recipes_factory.create("loadRegularPuzzle",
+                                                db=db,puzzle_num=puzzle_num,noise_level=gd_noise_level)
+        gd_puzzle_recipe.cook()
+        gd_recipe = recipes_factory.create("SD1Pairwise",puzzle=gd_puzzle_recipe.puzzle)
+        gd_graph_wrapper = gd_recipe.cook()
+
+        noise_level = 1
+        noisy_puzzle_recipe = recipes_factory.create("loadRegularPuzzle",
+                                                db=db,puzzle_num=puzzle_num,noise_level=noise_level)
+        noisy_puzzle_recipe.cook()
+        noisy_recipe = recipes_factory.create("SD1Pairwise",puzzle=noisy_puzzle_recipe.puzzle)
+        noisy_graph_wrapper = noisy_recipe.cook()
+
+        drawer = MatchingGraphDrawer(gd_graph_wrapper)
+        drawer.init()
+        # Because we you use the normalized dot product
+        min_edge_weight = -1
+        max_edge_weight = 1
+        drawer.draw_graph_matching(noisy_graph_wrapper,min_edge_weight=min_edge_weight,max_edge_weight=max_edge_weight)
+        drawer.draw_graph_filtered_matching(noisy_graph_wrapper,min_edge_weight=min_edge_weight,max_edge_weight=max_edge_weight)
+
+        plt.show()
+        
+
+
+
 
         
         
