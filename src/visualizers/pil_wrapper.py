c7c3d95ea4a6d8968bc520718a2cfd21446207ae
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-11-01 09:49:35 +0200

    deleting visualzation folder

--- src/visualizers/pil_wrapper.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-
-class Frame():
-    def __init__(self,file_path=None,size=(1024,1024),color=None):
-        self.img = np.zeros(size, np.uint8)
-        self.named_window = named_window
-        self.is_initialized = False
