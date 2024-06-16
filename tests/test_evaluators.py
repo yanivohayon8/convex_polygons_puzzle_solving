17fc42eeaffb2018d7c7631c8fe3ab03656b41d0
Yaniv Ohayon <yanivohayon8@gmail.com>
2024-06-16 08:44:15 +0300

    poc

--- tests/test_evaluators.py
+++ tests/test_evaluators.py
@@ -2,13 +2,13 @@ import unittest
 import sys
 sys.path.append("./")
 
-from src.evaluator import least_square_rigid_motion_svd,AreaOverlappingEvaluator
+from src.evaluator import least_square_rigid_motion_svd,AreaOverlappingEvaluator,registration_only_one_piece
 import numpy as np
 from src.data_types.puzzle import Puzzle
 from shapely import affinity
 from src import shared_variables
 import matplotlib.pyplot as plt
-from shapely import Polygon
+from shapely import Polygon,MultiPolygon
 
 import random
 
@@ -107,7 +107,7 @@ class TestAreaOverlappingEvaluator(unittest.TestCase):
     def plot(self,polygons, ax = None):
 
         if ax is None:
-            ax = plt.subplots()
+            _,ax = plt.subplots()
         
         random.seed(10)
 
@@ -184,7 +184,120 @@ class TestAreaOverlappingEvaluator(unittest.TestCase):
 
 
 
+    def test_translation_of_polygons(self):
+        db = "0-30"
+        puzzle_num = "19_DB1"
+        puzzle_noise_level = 0
+        puzzle_directory = f"../ConvexDrawingDataset/DB{db}/Puzzle{puzzle_num}/noise_{puzzle_noise_level}"
+        puzzle = Puzzle(puzzle_directory)
+        ground_truth_polygons = puzzle.get_ground_truth_puzzle()
 
+        tx = 0 # 0
+        ty = 0 # 0
+        angle = 50 #50
+        origin = MultiPolygon(ground_truth_polygons).centroid
+        translated_polygons = [affinity.rotate(affinity.translate(polygon,tx,ty),angle,origin=origin) for polygon in ground_truth_polygons]
+
+        self.plot(translated_polygons)
+
+        evaluator = AreaOverlappingEvaluator(ground_truth_polygons)
+        eval_score_perfect = evaluator.evaluate(translated_polygons)
+
+        self.plot(evaluator.transfomed_polygons_solution)
+        plt.show()
+
+
+    def moving_polygons_by_svd_(self,solution_polygons,ground_truth_polygons):
+        pieces_areas = list(map(lambda p: p.area,ground_truth_polygons))
+        largest_piece_index = pieces_areas.index(max(pieces_areas))
+        solution_largest_polygon = solution_polygons[largest_piece_index]
+        ground_truth_largest_polygon = ground_truth_polygons[largest_piece_index]
+
+        ground_truth_coords = np.array(solution_largest_polygon.exterior.coords)
+        ground_truth_truth_coords = np.array(ground_truth_largest_polygon.exterior.coords)
+
+        R,t = least_square_rigid_motion_svd(ground_truth_coords,ground_truth_truth_coords,np.ones(ground_truth_coords.shape[0]))
+            
+        translated_polygons = [affinity.translate(polygon,t[0],t[1]) for polygon in solution_polygons]
+        center_of_all = MultiPolygon(translated_polygons).centroid
+        angle = np.arccos(R[0,0])
+        rotated_polygons = [affinity.rotate(polygon,-angle,use_radians=True,origin=center_of_all) for polygon in translated_polygons]
+
+        return translated_polygons,rotated_polygons
+
+    def test_svd_reliablity(self):
+        db = "0-30"
+        puzzle_num = "19_DB1"
+        puzzle_noise_level = 0
+        puzzle_directory = f"../ConvexDrawingDataset/DB{db}/Puzzle{puzzle_num}/noise_{puzzle_noise_level}"
+        puzzle = Puzzle(puzzle_directory)
+        ground_truth_polygons = puzzle.get_ground_truth_puzzle()
+
+        tx = 3000 # 0
+        ty = 3000 # 0
+        angle = 50 #50
+        origin = MultiPolygon(ground_truth_polygons).centroid
+        shifted_polygons = [affinity.rotate(affinity.translate(polygon,tx,ty),angle,origin=origin) for polygon in ground_truth_polygons]
+
+        translated_polygons,rotated_polygons = self.moving_polygons_by_svd_(shifted_polygons,ground_truth_polygons)
+
+        _, axs = plt.subplots(2,2)
+        self.plot(ground_truth_polygons,axs[0,0])
+        axs[0,0].set_title("Ground Truth")
+        self.plot(shifted_polygons,axs[0,1])
+        axs[0,1].set_title("Shifted")
+        self.plot(translated_polygons,axs[1,0])
+        axs[1,0].set_title("translated")
+        self.plot(rotated_polygons,axs[1,1])
+        axs[1,1].set_title("rotated")
+
+        plt.show()
+
+        assert rotated_polygons[0].centroid.distance(ground_truth_polygons[0].centroid) < 1
+
+
+class TestRegistrationNoweights(unittest.TestCase):
+
+    def plot(self,polygons, ax = None):
+
+        if ax is None:
+            _,ax = plt.subplots()
+        
+        random.seed(10)
+
+        for poly in polygons:
+            xs,ys = poly.exterior.xy
+            ax.fill(xs,ys, alpha=0.5,fc=(random.random(),random.random(),random.random()),ec="black")
+
+        ax.set_aspect("equal")
+
+    def test_toy(self):
+        db = "0-30"
+        puzzle_num = "19_DB1"
+        puzzle_noise_level = 0
+        puzzle_directory = f"../ConvexDrawingDataset/DB{db}/Puzzle{puzzle_num}/noise_{puzzle_noise_level}"
+        puzzle = Puzzle(puzzle_directory)
+        ground_truth_polygons = puzzle.get_ground_truth_puzzle()
+
+        tx = 3000 # 0
+        ty = 3000 # 0
+        angle = 50 #50
+        origin = MultiPolygon(ground_truth_polygons).centroid
+        shifted_polygons = [affinity.rotate(affinity.translate(polygon,tx,ty),angle,origin=origin) for polygon in ground_truth_polygons]
+
+        q_pos,translated_polygons,rotated_polygons = registration_only_one_piece(shifted_polygons,ground_truth_polygons)
+
+        _, axs = plt.subplots(2,2)
+        self.plot(ground_truth_polygons,axs[0,0])
+        axs[0,0].set_title("Ground Truth")
+        self.plot(shifted_polygons,axs[0,1])
+        axs[0,1].set_title("Shifted")
+        self.plot(translated_polygons,axs[1,0])
+        axs[1,0].set_title("translated")
+        self.plot(rotated_polygons,axs[1,1])
+        axs[1,1].set_title("rotated")
+
+        plt.show()
 
 
 
